a70a7a352f82de3379a4b2977d99f2e11b181d61
Inseo Oh <dhdlstjtr@gmail.com>
2024-07-14 11:45:31 +0900

    initial commit
    may or may not work

---
+++ inc/script.js
@@ -0,0 +1,42 @@
+// Copyright 2024 Inseo Oh
+
+let oldCoords;
+let oldTime;
+
+function coordDiffInKm(a, b) {
+    const latDist = Math.abs(a.latitude - b.latitude);
+    const longDist = Math.abs(a.longitude - b.longitude);
+    // a와 b 중간 어딘가를 기준 위도로 사용
+    const lat = Math.min(a.latitude, b.latitude) + (latDist / 2.0);
+    // 1Lat : 110.574km = xLat = yKm
+    // 110.574km * xLat = yKm
+    const vertKmDist = latDist * 110.574;
+    const horiKmDist = longDist * (111.320 * Math.cos(lat * Math.PI));
+    const dist = Math.sqrt(vertKmDist * vertKmDist + horiKmDist * horiKmDist);
+    return dist;
+}
+
+function success(pos) {
+    // console.log(pos);
+    const currCoords = pos.coords;
+    const currTime = pos.timestamp;
+    if (oldCoords != undefined) {
+        const dist = coordDiffInKm(oldCoords, currCoords);
+        const timeDiff = currTime - oldTime; // 단위는 ms
+        const spd = (dist * 3600) / timeDiff;
+        document.querySelector('.speed').innerText = spd;
+    }
+    oldCoords = currCoords;
+    oldTime = currTime;
+    document.querySelector('.lat').innerText = pos.coords.latitude;
+    document.querySelector('.long').innerText = pos.coords.longitude;
+}
+function error(e) {
+    document.querySelector('.errbox').style.display = 'block';
+    document.querySelector('.errtext').innerText = `${e.message}\n(Code ${e.code})`;
+}
+
+window.onload = function() {
+    navigator.geolocation.watchPosition(success, error);
+
+}
